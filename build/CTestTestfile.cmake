31f1571d281f3a9309f34338399a4d3a62f85a63
Ilya Tsoy <inqlest@gmail.com>
2023-03-08 00:38:24 +0300

    cmake fix

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/vscode/HW-MF/Zad3
-# Build directory: C:/vscode/HW-MF/Zad3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
